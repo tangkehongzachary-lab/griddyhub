8f929d618886de7c4f36093a1807e1e810712f9e
tangkehongzachary-lab <tang_ke_hong_zachary@s2025.ssts.edu.sg>
2025-09-11 09:29:30 +0800

    Create ContactView.swift

---
+++ griddyhub/ContactView.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct ContactView: View {
+    var body: some View {
+        Text("Contact system is down as of now")
+        List {
+            Button("2ij8mhE") {
+                
+            }
+            Button("Add Contact") {
+                
+                
+            }
+        }
+        
+        
+    }
+    
+}
+#Preview {
+    ContactView()
+}
