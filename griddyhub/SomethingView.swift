711e5607347e0cc31b708d4170ed88c3f42d68ef
tangkehongzachary-lab <tang_ke_hong_zachary@s2025.ssts.edu.sg>
2025-09-11 09:27:14 +0800

    Update SomethingView.swift

--- griddyhub/SomethingView.swift
+++ griddyhub/SomethingView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct SomethingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("You Haven't Sent Anything!!")
     }
 }
 
