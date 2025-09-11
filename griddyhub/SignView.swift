4dfdeea2b9df6efdd0c3d05ad81797880696c2cd
tangkehongzachary-lab <tang_ke_hong_zachary@s2025.ssts.edu.sg>
2025-09-11 09:27:01 +0800

    Update SignView.swift

--- griddyhub/SignView.swift
+++ griddyhub/SignView.swift
@@ -1,12 +1,11 @@
-
 import SwiftUI
 
 struct SignView: View {
     @State private var inputText: String = ""
     var body: some View {
         VStack{
-        Text("Enter Username")
-        TextField("Username", text: $inputText)
+        Text("Enter Display Name (Can Change Any Time)")
+        TextField("Display Name", text: $inputText)
             .padding()
         NavigationStack {
             NavigationLink {
