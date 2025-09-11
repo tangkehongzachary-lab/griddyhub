f403fd73bc15b1ad8d0cfcd4ef51ef93cc26f5ca
tangkehongzachary-lab <tang_ke_hong_zachary@s2025.ssts.edu.sg>
2025-09-11 09:30:14 +0800

    Create SendView.swift

---
+++ griddyhub/SendView.swift
@@ -0,0 +1,31 @@
+
+import SwiftUI
+
+struct SendView: View {
+    @State private var inputText: String = ""
+    let specificString: String = "2ij8mhE"
+
+    var body: some View {
+        VStack {
+            TextField("To", text: $inputText)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            TextField("Message", text: $inputText)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+
+            Button("Send") {
+                if inputText == specificString {
+
+                    // Perform actions for a match
+                } else {
+                   
+                    // Perform actions for a mismatch
+                }
+            }
+        }
+    }
+}
+#Preview {
+    SendView()
+}
