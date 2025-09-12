10ffffc0ffafd6d7049d848e75dd480af692c777
tangkehongzachary-lab <tang_ke_hong_zachary@s2025.ssts.edu.sg>
2025-09-12 08:24:56 +0800

    Update GontentView.swift

--- griddyhub/GontentView.swift
+++ griddyhub/GontentView.swift
@@ -2,10 +2,12 @@ import SwiftUI
 
 struct GontentView: View {
     var body: some View {
-        Text("No Mail")
+        List{
+            Text("Placeholder for nav link")
+            }
+        }
     }
-}
-
 #Preview {
     GontentView()
 }
+
